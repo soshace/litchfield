eee2b09f8558b696a2bb7492ff91e41051483013
Nikita Bragin <bragin.na@vuaro.ru>
2014-10-23 18:36:30 +0400

    Slider added

--- js/script.js
+++ js/script.js
@@ -26,11 +26,47 @@ $(function () {
         $roleBlock.addClass('hide');
     }
 
-    $donateTab.on('click', function(){
+    function slider($slider, interval, speedTime) {
+        var $sliderItem = $('li', $slider),
+            sliderWidth = $sliderItem.outerWidth(),
+            slidersNumber = $sliderItem.length,
+            currentSliderNumber = 0;
+
+
+        setInterval(function () {
+            var $nextSliderItem,
+                $currentSliderItem,
+                nextItemNumber = currentSliderNumber + 1;
+
+            if (nextItemNumber === slidersNumber) {
+                nextItemNumber = 0;
+            }
+
+            $currentSliderItem = $($sliderItem[currentSliderNumber]);
+            $nextSliderItem = $($sliderItem[nextItemNumber]);
+            $nextSliderItem.css({'z-index': 1, left: sliderWidth});
+
+            $nextSliderItem.animate({
+                left: 0
+            });
+
+            $currentSliderItem.animate({
+                left: -sliderWidth
+            }, function () {
+                $currentSliderItem.css({'z-index': 0});
+            });
+
+            currentSliderNumber = nextItemNumber;
+        }, interval);
+    }
+
+    $donateTab.on('click', function () {
         switchTopBlock('donate');
     });
 
-    $roleTab.on('click', function(){
+    $roleTab.on('click', function () {
         switchTopBlock('role');
     });
+
+    slider($('.js-slider'), 2000, 500);
 });
